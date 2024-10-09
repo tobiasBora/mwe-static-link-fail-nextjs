9f973e92645db97b6dadd307cbd06f7cbcde81ee
Tobias Bora <tobias.bora@gmail.com>
2024-10-09 17:24:48 +0200

    Add link, enable static build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Link href="test">My test link</Link>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
